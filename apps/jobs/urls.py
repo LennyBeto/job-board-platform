b07863ec5f81708541c8b81a774f955bd4b47570
Lenny Beto <lennybeto95@gmail.com>
2025-11-07 21:46:01 +0300

    feat: configure URL routing and Swagger documentation

---
+++ apps/jobs/urls.py
@@ -0,0 +1,12 @@
+# apps/jobs/urls.py
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import JobViewSet, CategoryViewSet
+
+router = DefaultRouter()
+router.register(r'jobs', JobViewSet, basename='job')
+router.register(r'categories', CategoryViewSet, basename='category')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
